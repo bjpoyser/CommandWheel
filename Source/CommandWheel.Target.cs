668bed74dfd23b6483fe02fd3c8cadd1ca63d67d
Benoit Poyser <bj_hubs@hotmail.com>
2026-03-18 11:14:41 -0600

    Added Convai 3.6.9, Added option to change bot face color, fix issue of bot getting stuck after getting an orb

--- Source/CommandWheel.Target.cs
+++ Source/CommandWheel.Target.cs
@@ -8,8 +8,10 @@ public class CommandWheelTarget : TargetRules
 	public CommandWheelTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+        CppStandard = CppStandardVersion.Cpp20;
 
-		ExtraModuleNames.AddRange( new string[] { "CommandWheel" } );
+        ExtraModuleNames.AddRange( new string[] { "CommandWheel" } );
 	}
 }
--- Source/CommandWheelEditor.Target.cs
+++ Source/CommandWheelEditor.Target.cs
@@ -8,8 +8,10 @@ public class CommandWheelEditorTarget : TargetRules
 	public CommandWheelEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+        CppStandard = CppStandardVersion.Cpp20;
 
-		ExtraModuleNames.AddRange( new string[] { "CommandWheel" } );
+        ExtraModuleNames.AddRange( new string[] { "CommandWheel" } );
 	}
 }
